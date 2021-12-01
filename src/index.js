06ec17fefa2a3baa397368d91318bd09e731c945
benjp009 <patin.benjamin@gmail.com>
2021-12-01 18:23:05 +0100

    Start building list with set array

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
+const TASKS = [
+  {
+    description: 'Task 1',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Task 2',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Task 3',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Task 4',
+    completed: false,
+    index: 4,
+  },
+  {
+    description: 'Task 5',
+    completed: false,
+    index: 5,
+  },
+];
