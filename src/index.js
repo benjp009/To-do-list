7df9ffe01df9f99701df3dde905a53ce5b1078c8
benjp009 <patin.benjamin@gmail.com>
2021-12-01 07:03:31 +0100

    fix js linters

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
+  /*global someFunction, a*/
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
