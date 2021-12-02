c1f3209773bb0732643141931b4990ba07071182
benjp009 <patin.benjamin@gmail.com>
2021-12-02 18:23:20 +0100

    font awesome

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import _ from 'lodash';
 import './style.css';
+import Refresh from './refresh.png';
+
 
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const refreshIcon = new Image();
+  refreshIcon.src = Refresh;
+
+  element.appendChild(refreshIcon);
+
 
   return element;
 }
